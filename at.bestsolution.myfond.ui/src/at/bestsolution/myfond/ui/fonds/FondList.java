fa41dba243abf49d311fa9b8a2479f3b21c11e8b
Tom Schindl <tom.schindl@bestsolution.at>
2013-10-24 23:03:03 +0200

    edit currencies

--- at.bestsolution.myfond.ui/src/at/bestsolution/myfond/ui/fonds/FondList.java
+++ at.bestsolution.myfond.ui/src/at/bestsolution/myfond/ui/fonds/FondList.java
@@ -2,9 +2,14 @@ package at.bestsolution.myfond.ui.fonds;
 
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -12,10 +17,12 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.emf.databinding.EMFProperties;
 import org.eclipse.fx.core.databinding.AdapterFactory;
+import org.eclipse.fx.ui.databinding.ListUtil;
 import org.eclipse.fx.ui.databinding.PropertyListCellFaytory;
 
 import at.bestsolution.myfond.datasource.Datasource;
 import at.bestsolution.myfond.model.myfond.Fond;
+import at.bestsolution.myfond.model.myfond.MyfondFactory;
 import at.bestsolution.myfond.model.myfond.MyfondPackage;
 
 
@@ -26,6 +33,8 @@ public class FondList {
 	
 	@Inject
 	IEclipseContext context;
+
+	private ListView<Fond> listView;
 	
 	@PostConstruct
 	void init(BorderPane parent) {
@@ -33,10 +42,55 @@ public class FondList {
 		p.getStyleClass().add("viewContent");
 		p.setCenter(createList());
 		parent.setCenter(p);
+		
+		HBox box = new HBox(10);
+		box.setPadding(new Insets(10,10,0,10));
+		
+		{
+			Region spacer = new Region();
+			HBox.setHgrow(spacer, Priority.ALWAYS);
+			box.getChildren().add(spacer);
+		}
+		
+		{
+			Button b = new Button();
+			b.getStyleClass().addAll("toolbutton","add");
+			b.setOnAction((e) -> addEntry());
+			box.getChildren().add(b);
+		}
+		
+		{
+			Button b = new Button();
+			b.getStyleClass().addAll("toolbutton","remove");
+			b.setOnAction((e) -> removeEntry()); 
+			box.getChildren().add(b);
+		}
+		
+		{
+			Region spacer = new Region();
+			HBox.setHgrow(spacer, Priority.ALWAYS);
+			box.getChildren().add(spacer);
+		}
+		
+		p.setBottom(box);
+	}
+	
+	private void addEntry() {
+		Fond fond = MyfondFactory.eINSTANCE.createFond();
+		datasource.getRoot().getFondList().add(fond);
+		listView.getSelectionModel().select(fond);
+	}
+	
+	private void removeEntry() {
+		datasource.getRoot().getFondList().remove(listView.getSelectionModel().getSelectedItem());
+		listView.getSelectionModel().select(datasource.getRoot().getFondList().get(0));
 	}
 	
 	private Node createList() {
-		ListView<Fond> listView = new ListView<>();
+		listView = new ListView<>();
+//		ListUtil.setupList(listView, property);
+		
+		
 		listView.setCellFactory(PropertyListCellFaytory.<Fond>textFactory(EMFProperties.value(MyfondPackage.Literals.FOND__NAME)));
 		listView.setItems(AdapterFactory.<Fond>adapt(EMFProperties.list(MyfondPackage.Literals.MY_FOND__FOND_LIST).observe(datasource.getRoot())));
 		listView.getSelectionModel().selectedItemProperty().addListener(new InvalidationListener() {
--- at.bestsolution.myfond.ui/src/at/bestsolution/myfond/ui/fonds/controllers/CurrencyController.java
+++ at.bestsolution.myfond.ui/src/at/bestsolution/myfond/ui/fonds/controllers/CurrencyController.java
@@ -5,31 +5,54 @@ import static at.bestsolution.myfond.model.myfond.MyfondPackage.Literals.CURRENC
 import static at.bestsolution.myfond.model.myfond.MyfondPackage.Literals.CURRENCY__NAME;
 import static at.bestsolution.myfond.model.myfond.MyfondPackage.Literals.FOND__CURRENCY_DISTRIBUTION_LIST;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 import javax.inject.Inject;
 
 import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.emf.databinding.EMFProperties;
 import org.eclipse.emf.databinding.FeaturePath;
 import org.eclipse.fx.core.databinding.AdapterFactory;
 import org.eclipse.fx.ui.databinding.TableUtil;
+import org.eclipse.fx.ui.di.FXMLLoader;
+import org.eclipse.fx.ui.di.FXMLLoaderFactory;
 
 import at.bestsolution.myfond.model.myfond.CurrencyDistribution;
 import at.bestsolution.myfond.model.myfond.Fond;
+import at.bestsolution.myfond.model.myfond.MyfondFactory;
 
 public class CurrencyController implements Initializable {
 	private WritableValue master = new WritableValue();
+	
+	@Inject
+	@FXMLLoader
+	FXMLLoaderFactory factory;
+	
 	@FXML TableView<CurrencyDistribution> table;
 	@FXML TableColumn<CurrencyDistribution,CurrencyDistribution> name;
 	@FXML TableColumn<CurrencyDistribution,CurrencyDistribution> value;
+	
+	@Inject
+	IEclipseContext context;
 	
 	@Inject
 	public void setFond(@Optional Fond fond) {
@@ -40,16 +63,51 @@ public class CurrencyController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		TableUtil.setupColumn(name, "{0}", EMFProperties.value(FeaturePath.fromList(CURRENCY_DISTRIBUTION__CURRENCY,CURRENCY__NAME)));
 		TableUtil.setupColumn(value, "{0,number,#,##0.00}", EMFProperties.value(FeaturePath.fromList(CURRENCY_DISTRIBUTION__PERCENTAGE)));
-//		Callback<TableColumn<CurrencyDistribution,CurrencyDistribution>, TableCell<CurrencyDistribution,CurrencyDistribution>> cb = value.getCellFactory();
-//		value.setCellFactory(new Callback<TableColumn<CurrencyDistribution,CurrencyDistribution>, TableCell<CurrencyDistribution,CurrencyDistribution>>() {
-//			
-//			@Override
-//			public TableCell<CurrencyDistribution, CurrencyDistribution> call(
-//					TableColumn<CurrencyDistribution, CurrencyDistribution> param) {
-//				TableCell<CurrencyDistribution, CurrencyDistribution> c = cb.call(param);
-//				return c;
-//			}
-//		});
 		table.setItems(AdapterFactory.<CurrencyDistribution>adapt(EMFProperties.list(FOND__CURRENCY_DISTRIBUTION_LIST).observeDetail(master)));
+		table.getSelectionModel().selectedItemProperty().addListener(new InvalidationListener() {
+			
+			@Override
+			public void invalidated(Observable observable) {
+				context.set(CurrencyDistribution.class, table.getSelectionModel().getSelectedItem());
+			}
+		});
+	}
+
+	@FXML public void onAddCurrency() {
+		Fond f = (Fond) master.getValue();
+		CurrencyDistribution c = MyfondFactory.eINSTANCE.createCurrencyDistribution();
+		f.getCurrencyDistributionList().add(c);
+		table.getSelectionModel().select(c);
+	}
+
+	@FXML public void onEditCurrency() {
+		Stage s = new Stage(StageStyle.TRANSPARENT);
+		
+		Window window = table.getScene().getWindow();
+		s.initOwner(window);
+		
+		double cx = window.getX() + (window.getWidth() / 2);
+		double cy = window.getY() + (window.getHeight() / 2);
+		
+		s.setX(cx-100);
+		s.setY(cy-100);
+		
+		try {
+			Parent root = (Parent) factory.loadBundleRelative("/at/bestsolution/myfond/ui/fonds/CurrencyDialog.fxml").load();
+			Scene sc = new Scene(root,400,-1);
+			sc.setFill(Color.TRANSPARENT);
+			sc.getStylesheets().addAll(window.getScene().getStylesheets());
+			s.setScene(sc);
+			s.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML public void onRemoveCurrency() {
+		Fond f = (Fond) master.getValue();
+		f.getCurrencyDistributionList().remove(table.getSelectionModel().getSelectedItem());
+		table.getSelectionModel().select(f.getCurrencyDistributionList().get(0));
 	}
 }
