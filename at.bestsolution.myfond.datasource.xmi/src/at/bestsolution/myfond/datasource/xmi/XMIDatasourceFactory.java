981eb43370435f9d7f10b5bbd91b2df277320ac0
Tom Schindl <tom.schindl@bestsolution.at>
2013-10-24 15:24:33 +0200

    remove bins

--- at.bestsolution.myfond.datasource.xmi/src/at/bestsolution/myfond/datasource/xmi/XMIDatasourceFactory.java
+++ at.bestsolution.myfond.datasource.xmi/src/at/bestsolution/myfond/datasource/xmi/XMIDatasourceFactory.java
@@ -1,24 +1,36 @@
 package at.bestsolution.myfond.datasource.xmi;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
 import at.bestsolution.myfond.datasource.Datasource;
 import at.bestsolution.myfond.datasource.DatasourceFactory;
 import at.bestsolution.myfond.model.myfond.MyFond;
 
 public class XMIDatasourceFactory implements DatasourceFactory {
-
+	private Resource resource;
+	
 	@Override
 	public Datasource createDatasource() {
-		// TODO Auto-generated method stub
-		return null;
+		if( resource == null ) {
+			ResourceSet resourceSet = new ResourceSetImpl();
+			resource = resourceSet.getResource(URI.createPlatformPluginURI("/at.bestsolution.myfond.datasource.xmi/MyFond.xmi", true), true);
+		}
+		return new DatasourceImpl(resource);
 	}
 
 	static class DatasourceImpl implements Datasource {
-
-		@Override
-		public MyFond getRoot() {
-			// TODO Auto-generated method stub
-			return null;
+		private Resource resource;
+		
+		public DatasourceImpl(Resource resource) {
+			this.resource = resource;
 		}
 		
+		@Override
+		public MyFond getRoot() {
+			return (MyFond) resource.getContents().get(0);
+		}
 	}
 }
