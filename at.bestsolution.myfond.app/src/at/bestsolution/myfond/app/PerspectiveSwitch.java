b7151cdbc337b932a6b4d617506d1e4ade909476
Tom Schindl <tom.schindl@bestsolution.at>
2013-10-27 22:38:08 +0100

    fixed ignores

---
+++ at.bestsolution.myfond.app/src/at/bestsolution/myfond/app/PerspectiveSwitch.java
@@ -0,0 +1,15 @@
+package at.bestsolution.myfond.app;
+
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.fx.ui.workbench.renderers.base.services.PerspectiveTransitionService;
+
+public class PerspectiveSwitch implements PerspectiveTransitionService {
+
+	@Override
+	public AnimationDelegate getDelegate(MPerspective fromPerspective,
+			MPerspective toPerspective) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
